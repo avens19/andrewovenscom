6ce383c414f17f8058ed576e6c5d27daf9ff8f8b
Andrew Ovens <avens19@gmail.com>
2018-01-31 22:58:13 -0700

    Don't check known hosts

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -x
 
 mkdir .ssh
 echo $PRIVATE_KEY > .ssh/www-data
-scp -i .ssh/www-data -r dist/* www-data@andrewovens.com:/var/www/html/
+scp -i .ssh/www-data -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r dist/* www-data@andrewovens.com:/var/www/html/
