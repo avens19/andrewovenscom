64c503bc27aa542f54ce73c950123467b9e7cea7
Andrew Ovens <avens19@gmail.com>
2018-01-31 23:50:02 -0700

    Use deploy user

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 set -x
 
 mkdir .ssh
-echo $PRIVATE_KEY > .ssh/www-data
-chmod 600 .ssh/www-data
-scp -i .ssh/www-data -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r dist/* www-data@andrewovens.com:/var/www/html/
+echo $PRIVATE_KEY > .ssh/deploy
+chmod 600 .ssh/deploy
+scp -i .ssh/deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r dist/* deploy@andrewovens.com:/var/www/html/
