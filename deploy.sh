1466fb013f695d41fac07e3c6f69d0bcc9b99fe6
Andrew Ovens <avens19@gmail.com>
2018-01-31 23:12:04 -0700

    Protect private key file

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ set -x
 
 mkdir .ssh
 echo $PRIVATE_KEY > .ssh/www-data
+chmod 600 .ssh/www-data
 scp -i .ssh/www-data -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r dist/* www-data@andrewovens.com:/var/www/html/
